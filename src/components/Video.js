8562adc17c5b2e7792884a76206209cc72fd811a
bgeck <benjamin.geck@gmail.com>
2019-11-08 19:49:31 -0700

    video embeds

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,24 +2,34 @@ import React from "react"
 import styled from "styled-components"
 
 const Wrapper = styled.div`
-  display: block;
+  position: relative;
+  padding-bottom: 56.25%;
+  height: 0;
+  overflow: hidden;
+  max-width: 100%;
+  background: ${props => props.theme.colorLight};
 `
 
 const VideoFrame = styled.iframe`
-  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `
 
-const Video = ({ url, title }) => (
-  <Wrapper className="video">
-    <VideoFrame
-      src={url}
-      title={title}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
-      allowFullScreen
-    />
-  </Wrapper>
-)
+const Video = ({ type, url }) => {
+  return (
+    <Wrapper className="video">
+      {type === "youtube" && (
+        <VideoFrame
+          src={`https://www.youtube-nocookie.com/embed/${url}`}
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          frameBorder="0"
+          allowFullScreen
+        />
+      )}
+    </Wrapper>
+  )
+}
 export default Video
