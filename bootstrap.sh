77f9e9c0c2d47fb37f035750cb3feec604b9b53e
Benjamin Geck <benjamin.geck@gmail.com>
2021-07-26 12:09:06 -0600

    update bootstrap code

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,48 +3,26 @@ set -e
 
 echo "  0% -- Setting Environment Variables"
 export DEBIAN_FRONTEND=noninteractive
-export ANDROID_SDK_ROOT=/home/vagrant/Android/sdk
-export PATH="$PATH:/home/vagrant/Android/sdk/platform-tools"
-export PATH="$PATH:/home/vagrant/flutter/bin"
-
-# permanently set for future logins
-sudo tee "/etc/profile.d/flutter.sh" > "/dev/null" <<EOF
-export ANDROID_SDK_ROOT=/home/vagrant/Android/sdk
-export PATH="$PATH"
-cd /home/vagrant/projects
-EOF
 
 echo "  5% -- Installing Needed Utilities"
 {
 sudo apt-get update 
-sudo apt-get install -y openjdk-8-jdk wget unzip git
-} &> /dev/null
+sudo apt-get install -y git curl
+}
 
-echo " 40% -- Initializing Workspace"
+echo " 40% -- Installing NVM and Node"
 {
-sudo rm -rf Android .android flutter
-mkdir -p Android/sdk .android && touch .android/repositories.cfg
-} &> /dev/null
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
+}
 
-echo " 45% -- Downloading Android SDK"
+echo " 45% -- Installing NPM Packages"
 {
-wget -q -O sdk-tools.zip https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-unzip sdk-tools.zip && rm sdk-tools.zip
-mv tools Android/sdk/tools
-} &> /dev/null
-
-echo " 55% -- Installing Android SDK"
-{
-cd Android/sdk/tools/bin && yes | ./sdkmanager --licenses
-./sdkmanager "build-tools;30.0.3" "patcher;v4" "platform-tools" "platforms;android-30" "sources;android-30" "cmdline-tools;latest"
-} &> /dev/null
-
-echo " 70% -- Installing Flutter"
-{
-cd /home/vagrant
-git clone https://github.com/flutter/flutter.git
-flutter doctor --android-licenses
-flutter doctor
-} &> /dev/null
+cd /vagrant
+npm install
+}
 
 echo "100% -- Ready to Roll!"
